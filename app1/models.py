a6f6b76e7f95a5be426ae7fe50fccf712213714b
Kushal Baral <136962554+kushal1o1@users.noreply.github.com>
2023-12-25 18:27:57 +0545

    v1

---
+++ app1/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Code(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255,default='UNKNOWN')
+    code = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
---
+++ app1/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render ,redirect
+from django.http import HttpResponseForbidden
+from .models import Code
+PASSWORD = "101"
+def home(request):
+    if request.method == 'POST':
+        # Check if the password is provided in the form
+        password = request.POST.get('password', '')
+        if password == PASSWORD:
+            # Password is correct, proceed to save the text
+            name = request.POST.get('name', '')
+            if name=='':
+             name="Unknown"
+            text = request.POST.get('inputText', '')
+            if text:
+              Code.objects.create(code=text, name=name)
+
+            # Check if the number of objects exceeds the limit (e.g., 10)
+            if Code.objects.count() > 10:
+                # Delete the oldest object (the first created)
+                oldest_code = Code.objects.first()
+                oldest_code.delete()
+
+            return redirect('home')
+
+        
+        else:
+           return render(request, 'app1/access_denied.html')
+
+    codes = Code.objects.all().order_by('-created_at')
+    parms = {'codes': codes}
+    return render(request, 'app1/index.html', parms)
